f509f4dafe146e2c6c79f031fa821d0b004b2446
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2021-03-09 01:02:24 +0100

    Setup error handling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,39 @@
 import frontend.parser.Fula;
 import frontend.ast.Ast;
 
-import cats.effect.IO;
+import cats.effect._;
+import cats.data.EitherT;
 import fastparse._
 import scala.io.Source;
 
 object Main {
 
-	def readFile(filename: String) : IO[String] = IO {
-		scala.io.Source.fromFile(filename).mkString
+	def readFile(filename: String) : IO[Either[Unit,String]] = IO {
+		try {
+		Right(scala.io.Source.fromFile(filename).mkString)
+		} 
+		catch {case e: Exception => Left(println("No file named " + filename))}
 	}
 	
-	def get_tree(prog: String): Ast.Prog = {
-		val tree = fastparse.parse(prog, Fula.Prog(_)).get
-		tree.value
+	def get_tree(prog: String): IO[Either[Unit,Ast.Prog]] = IO {
+		try {
+			val tree = fastparse.parse(prog, Fula.Prog(_)).get 
+			Right(tree.value) 
+		} 
+		catch { case e: Exception => Left(println("Syntax error")) }
 	}
 
-	def putTree(tree: Ast.Prog) : IO[Unit] = IO { println(tree) }
-
-	def putStrLn(line: String): IO[Unit] = IO { println(line) }
-
-	def manipulate_ars(args: Array[String]) = args.length 
+	def get_ast(filename: String) : EitherT[IO,Unit,Ast.Prog] = for {
+		prog <- EitherT(readFile(filename))
+		tree <- EitherT(get_tree(prog))
+	} yield tree
 
 	def main(args: Array[String]): Unit = {
-		val prog = for {
-			file <- readFile(args(0))
-			tree = get_tree(file)
-			_ <- putTree(tree)
+		val prog: IO[Unit] = for {
+		_ <- get_ast(args(0)).value.flatMap({
+						case Right(tree) => IO(println(tree))
+						case Left(error) => IO(error)
+				})
 		} yield ()
 
 		prog.unsafeRunSync()
